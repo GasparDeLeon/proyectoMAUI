6ebaf999254317656cc64e48b376b6496e64e77f
GasparDeLeon <gaspardeleon00@gmail.com>
2025-08-23 06:15:48 -0300

    agregue icons al flyout

--- ObligatorioTT/MauiProgram.cs
+++ ObligatorioTT/MauiProgram.cs
@@ -29,6 +29,7 @@ namespace ObligatorioTT
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("Arial.ttf", "ArialAlias");
+                    fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcons");
                 });
 
 #if ANDROID || IOS
